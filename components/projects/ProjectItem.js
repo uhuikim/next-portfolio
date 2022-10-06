0c3d5e6698b47995682f11e62887ccbb1e0f6ee4
blockodyssey-uhui <reah@blockodyssey.io>
2022-10-06 11:10:38 +0900

    feat: 노션 api 연결

---
+++ components/projects/ProjectItem.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Image from "next/image";
+
+const ProjectItem = ({ data }) => {
+    const {
+        properties: { Name, Github, Youtube, Description, Tags, WorkPeriod },
+    } = data;
+
+    const title = Name.title[0]?.plain_text;
+    const github = Github?.url;
+    const youtube = Youtube?.url;
+    const description = Description.rich_text[0]?.plain_text;
+    const imgSrc = data.cover.file?.url || data.cover.external.url || "";
+    const tags = Tags.multi_select;
+    const start = WorkPeriod.date?.start;
+    const end = WorkPeriod.date?.end;
+
+    const calculatedPeriod = (start, end) => {
+        const startDateStringArray = start.split("-");
+        const endDateStringArray = end.split("-");
+
+        var startDate = new Date(startDateStringArray[0], startDateStringArray[1], startDateStringArray[2]);
+        var endDate = new Date(endDateStringArray[0], endDateStringArray[1], endDateStringArray[2]);
+
+        const diffInMs = Math.abs(endDate - startDate);
+        const result = diffInMs / (1000 * 60 * 60 * 24);
+        return result;
+    };
+
+    return (
+        <div className="project-card">
+            <Image className="rounded-t-xl" src={imgSrc} alt="cover image" width="100%" height="50%" layout="responsive" objectFit="cover" quality={100} />
+
+            <div className="p-4 flex flex-col">
+                <h1 className="text-2xl font-bold">{title}</h1>
+                <h3 className="mt-4 text-xl">{description}</h3>
+                <a href={github}>깃허브 바로가기</a>
+                <a href={youtube}>유튜브 시연영상 보러가기</a>
+                <p className="my-1 ">
+                    작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
+                </p>
+                <div className="flex items-start mt-2">
+                    {tags.map((aTag) => (
+                        <h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" key={aTag.id}>
+                            {aTag.name}
+                        </h1>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectItem;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ["www.notion.so", "images.unsplash.com", "s3.us-west-2.amazonaws.com"],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
