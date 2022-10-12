665d8d365eead8ce75e40e8434e712ae7440ff03
blockodyssey-uhui <reah@blockodyssey.io>
2022-10-12 09:43:41 +0900

    feat: 노션 링크 추가

--- components/projects/ProjectItem.js
+++ components/projects/ProjectItem.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const ProjectItem = ({ data }) => {
     const {
-        properties: { Name, Github, Youtube, Description, Tags, WorkPeriod },
+        properties: { Name, Github, Youtube, Description, Tags, WorkPeriod, URL },
     } = data;
 
     const title = Name.title[0]?.plain_text;
@@ -29,24 +30,25 @@ const ProjectItem = ({ data }) => {
 
     return (
         <div className="project-card">
-            <Image className="rounded-t-xl" src={imgSrc} alt="cover image" width="100%" height="50%" layout="responsive" objectFit="cover" quality={100} />
-
-            <div className="p-4 flex flex-col">
-                <h1 className="text-2xl font-bold">{title}</h1>
-                <h3 className="mt-4 text-xl">{description}</h3>
-                <a href={github}>깃허브 바로가기</a>
-                <a href={youtube}>유튜브 시연영상 보러가기</a>
-                <p className="my-1 ">
-                    작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
-                </p>
-                <div className="flex items-start mt-2">
-                    {tags.map((aTag) => (
-                        <h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" key={aTag.id}>
-                            {aTag.name}
-                        </h1>
-                    ))}
+            <a href={URL.url} target="_blank" rel="noopener noreferrer">
+                <Image className="rounded-t-xl" src={imgSrc} alt="cover image" width="100%" height="50%" layout="responsive" objectFit="cover" quality={100} />
+                <div className="p-4 flex flex-col">
+                    <h1 className="text-2xl font-bold">{title}</h1>
+                    <h3 className="mt-4 text-xl">{description}</h3>
+                    <a href={github}>깃허브 바로가기</a>
+                    <a href={youtube}>유튜브 시연영상 보러가기</a>
+                    <p className="my-1 ">
+                        작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
+                    </p>
+                    <div className="flex items-start mt-2">
+                        {tags.map((aTag) => (
+                            <h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" key={aTag.id}>
+                                {aTag.name}
+                            </h1>
+                        ))}
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     );
 };
