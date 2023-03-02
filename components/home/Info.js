ac60498a17cce31e2c7267c9c5e04f3246f7d7c8
blockodyssey-uhui <reah@blockodyssey.io>
2023-03-03 08:50:15 +0900

    bug : 버그 수정

--- components/home/Info.js
+++ components/home/Info.js
@@ -8,9 +8,8 @@ const Info = () => {
             <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                        안녕하세요 빡빡이입니다!
+                        안녕하세요 프론트엔드 개발자 김우희입니다.
                         <br className="hidden lg:inline-block" />
-                        오늘도 빡코딩?
                     </h1>
                     <p className="mb-8 leading-relaxed">
                         기관과 웅대한 자신과 속에 것이다. 밝은 물방아 간에 안고, 따뜻한 무엇이 얼마나 우리 칼이다. 부패를 천고에 뭇 것은 인생을 천자만홍이 원대하고, 이상 뛰노는 것이다. 있을 그러므로
--- components/projects/ProjectItem.js
+++ components/projects/ProjectItem.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 const ProjectItem = ({ data }) => {
+    const [mounted, setMounted] = useState(false);
+
     const {
-        properties: { Name, Github, Youtube, Description, Tags, WorkPeriod, URL },
+        properties: { Name, GitHub, Youtube, Description, Tags, WorkPeriod, URL },
     } = data;
 
     const title = Name.title[0]?.plain_text;
-    const github = Github?.url;
+    const github = GitHub?.url;
     const youtube = Youtube?.url;
     const description = Description.rich_text[0]?.plain_text;
     const imgSrc = data.cover.file?.url || data.cover.external.url || "";
@@ -28,28 +30,41 @@ const ProjectItem = ({ data }) => {
         return result;
     };
 
+    useEffect(() => {
+        setMounted(true);
+    }, []);
     return (
-        <div className="project-card">
-            <a href={URL.url} target="_blank" rel="noopener noreferrer">
-                <Image className="rounded-t-xl" src={imgSrc} alt="cover image" width="100%" height="50%" layout="responsive" objectFit="cover" quality={100} />
-                <div className="p-4 flex flex-col">
-                    <h1 className="text-2xl font-bold">{title}</h1>
-                    <h3 className="mt-4 text-xl">{description}</h3>
-                    <a href={github}>깃허브 바로가기</a>
-                    <a href={youtube}>유튜브 시연영상 보러가기</a>
-                    <p className="my-1 ">
-                        작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
-                    </p>
-                    <div className="flex items-start mt-2">
-                        {tags.map((aTag) => (
-                            <h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" key={aTag.id}>
-                                {aTag.name}
-                            </h1>
-                        ))}
+        mounted && (
+            <div className="project-card">
+                <a href={URL.url} target="_blank" rel="noopener noreferrer">
+                    <Image className="rounded-t-xl" src={imgSrc} alt="cover image" width="100%" height="50%" layout="responsive" objectFit="cover" quality={100} />
+                    <div className="p-4 flex flex-col">
+                        <h1 className="text-2xl font-bold">{title}</h1>
+                        <h3 className="mt-4 text-xl">{description}</h3>
+                        {github && (
+                            <a rel="noopener noreferrer" target="_blank" href={github}>
+                                깃허브 바로가기
+                            </a>
+                        )}
+                        {youtube && (
+                            <a rel="noopener noreferrer" target="_blank" href={youtube}>
+                                유튜브 시연영상 보러가기
+                            </a>
+                        )}
+                        <p className="my-1 ">
+                            작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
+                        </p>
+                        <div className="flex items-start mt-2">
+                            {tags.map((aTag) => (
+                                <h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" key={aTag.id}>
+                                    {aTag.name}
+                                </h1>
+                            ))}
+                        </div>
                     </div>
-                </div>
-            </a>
-        </div>
+                </a>
+            </div>
+        )
     );
 };
 
